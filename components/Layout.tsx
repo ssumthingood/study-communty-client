fb030d9c5955403eca281e58ed4d9c1ef2ed2e11
ssumthingood <ssumthingood@gmail.com>
2022-09-17 23:58:24 +0900

    add resize hook in layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,9 +1,11 @@
 import { cls } from "@libs/client/utils";
+import useResize from "@libs/hooks/useResize";
 
 interface LayoutProps {
     children: React.ReactNode;
 }
 
 export default function Layout({ children }: LayoutProps) {
+    const size = useResize();
     return <div>{children}</div>;
 }
--- components/SmLayout.tsx
+++ components/SmLayout.tsx
@@ -1,9 +1,11 @@
 import { cls } from "@libs/client/utils";
+import useResize from "@libs/hooks/useResize";
 
 interface LayoutProps {
     children: React.ReactNode;
 }
 
 export default function SmLayout({ children }: LayoutProps) {
+    const size = useResize();
     return <>{children}</>;
 }
